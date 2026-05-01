dadbb214887e629069f26b595bb07164b3d409d5
Juljan Sina <jsina01@icloud.com>
2026-05-01 15:13:50 +0200

    fix gemini model name

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -126,7 +126,7 @@ export default function AddProduct() {
       const GEMINI_KEY = "AIzaSyAeAZ-IgdpUZSoDquHrWxcRoAKVBSVqDVo";
 
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_KEY,
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + GEMINI_KEY,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
